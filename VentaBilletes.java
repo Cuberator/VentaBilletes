7772cc63618eb2e1cfd677c2cef8eb55dc9c5246
Cuberator <61475731+Cuberator@users.noreply.github.com>
2020-03-02 18:12:25 +0100

    Create VentaBilletes.java

---
+++ VentaBilletes.java
@@ -0,0 +1,60 @@
+/**
+ * VentaBilletes.java
+ * @version Protoipo 0
+ * @author Mario Latorre y David Monfort (02/2020)
+ * 
+ */
+
+
+public class VentaBilletes {
+    private Autobus[] autobuses;
+    private Conductor[] conductores;
+    private Viaje[] viajes;
+    private Viajero[] viajeros;
+    
+    public VentaBilletes(){
+        
+    }
+    
+    public void asignar(Conductor conductor){
+        
+    }
+    
+    public void asignar(Autobus autobus){
+        
+    }
+    
+    public void modificar(Conductor conductor){
+        
+    }
+    
+    public void modificar(Autobus autobus){
+        
+    }
+    
+    public void anyadir(Conductor conductor){
+        
+    }
+    
+    public void anyadir(Autobus autobus){
+        
+    }
+    
+    public void anyadir(Viajero viajero){
+        
+    }
+    
+    public void asignarAsientos(){
+        
+    }
+    
+    public void mostrarOcupacion(){
+        
+    }
+    
+    public void elegirViaje(){
+        
+    }
+    
+    
+}
