64f7bc4c8c7deb9678ac93f2bb65dafa2b687e9d
Cuberator <61475731+Cuberator@users.noreply.github.com>
2020-03-02 18:12:51 +0100

    Create Viajero.java

---
+++ Viajero.java
@@ -0,0 +1,21 @@
+/**
+ * .java
+ * @version Protoipo 0
+ * @author Mario Latorre y David Monfort (02/2020)
+ * 
+ */
+public class Viajero {
+    private String nombre;
+    private String dni;
+    
+    public Viajero() {
+        
+    }
+    
+    public void escogerViaje(){
+        
+    }
+    
+    
+            
+}
